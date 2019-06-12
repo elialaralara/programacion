f24d5a58b7df16adf2392c659b26320a6ea620e7
elialaralara <50604047+elialaralara@users.noreply.github.com>
2019-06-12 16:24:35 -0500

    separador
    en este programa lo que hace es que el usuario digite un texto y el programa lo lea pero cada ves que aiga un espacio el programa separe laspalabras que siguen un reglon abajo y asi con cada una de las palabras

---
+++ string.cpp
@@ -0,0 +1,29 @@
+//UNIVERSIDAD POLITECNICA DE TULANCINGO
+//PROF: ARTURO NEGRETE MEDELLIN
+//ALUMNA:ELIA AMOR LARA CABAÑAS
+//MATRICULA:1830995
+//3ER CUATRIMESTRE
+//PRIMER PARCIAL
+
+#include <string>
+#include<iostream>
+using namespace std;
+int main()
+{
+
+	string cadena, token;
+	cout<<"captura una cadena: ";
+	getline(cin, cadena);
+	//cout<<"la cadena mide: "<<cadena.length()<<"bytes."<<endl;
+	string delimita=" ";
+	size_t pos=0;
+	while((pos=cadena.find(delimita))!= string::npos)
+	{
+		token=cadena.substr(0,pos);
+		cout<<token<<endl;
+		cadena.erase(0,pos+delimita.length());
+	}
+	cout<<cadena;
+	return 0;
+}
+//HOMEWORK  armar un arrelo para que me lo compile odo junto en la parte de abajo
